2f711a7eb083375ea7fee0755ad14fdd6e10060d
zvdas <rodriguezzude@gmail.com>
2022-08-31 22:37:23 +0530

    angular app built and static views set in backend

--- backend_nodejs_v1/server.js
+++ backend_nodejs_v1/server.js
@@ -23,7 +23,7 @@ const contacts = require('./routes/contact.route');
 const app = express();
 
 // use body parser
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
 
 // enable cors
 app.use(cors());
