d6313afa7968822f96b48f21831827aeb5a72e8b
zvdas <rodriguezzude@gmail.com>
2022-09-02 09:25:49 +0530

    README.md added to backend folder

---
+++ backend_nodejs_v1/README.md
@@ -0,0 +1,1 @@
+This backend folder is not in use by the project. The frontend is hosted on firebase.
--- backend_nodejs_v1/server.js
+++ backend_nodejs_v1/server.js
@@ -29,7 +29,13 @@ app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 // enable cors
-app.use(cors());
+// app.use(cors());
+app.use(cors({
+    'origin': '*',
+    'methods': 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    'preflightContinue': false,
+    'optionsSuccessStatus': 204
+}));
 // app.use(cors({ origin: process.env.ORIGIN }));
 
 // connect database
